de23bb0ff1b57b6a0bc786d3475d1fa2ef0e661f
Amy Posten <amyposten85@gmail.com>
2021-11-04 09:35:20 -0400

    commit tree e9ddb83b4a89af9a19123149b87c55637c00210c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+    array.join(" and ") if array.length < 3
 
+
+    if array.count == 1
+        return that item as a string
+                string = array.join()
+                string
+        
+            elsif array.count ==2
+                string = array.join(" and ")
+                string
+        
+            else
+                string = array[0..-2].join(", ")
+                string << ", and"
+                string << " #{array[-1]}"
+                string
+            end
 end
